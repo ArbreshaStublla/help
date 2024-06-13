47447d419ecf2f7a01d03e93d6f0d4f0b2b76a80
albulenamakolli1 <96053284+albulenamakolli1@users.noreply.github.com>
2024-06-13 10:45:07 +0200

    ..

--- src/components/ArticleList.vue
+++ src/components/ArticleList.vue
@@ -1,39 +1,39 @@
 <template>
   <v-container>
-  <div class="container" :class="{ 'right-panel-active': activeTab === 'signup' }">
-    <div class="form-container sign-up-container">
-      <h1>Create Account</h1>
-      <form @submit.prevent="registerUser">
-        <input type="text" placeholder="Username" v-model="signupData.username" required />
-        <input type="email" placeholder="Email" v-model="signupData.email" required />
-        <input type="password" placeholder="Password" v-model="signupData.password" required />
-        <button type="submit">Sign Up</button>
-      </form>
-    </div>
-    <div class="form-container sign-in-container">
-      <h1>Sign in</h1>
-      <form @submit.prevent="loginUser">
-        <input type="email" placeholder="Email" v-model="loginData.email" required />
-        <input type="password" placeholder="Password" v-model="loginData.password" required />
-        <button type="submit">Sign In</button>
-      </form>
-    </div>
-    <div class="overlay-container">
-      <div class="overlay">
-        <div class="overlay-panel overlay-left">
-          <h1>Welcome Back!</h1>
-          <p>To keep connected with us please login with your personal info</p>
-          <button class="ghost" @click="toggleTab('login')">Sign In</button>
-        </div>
-        <div class="overlay-panel overlay-right">
-          <h1>Hello, Explorer!</h1>
-          <p>Enter your personal details and start journey with us</p>
-          <button class="ghost" @click="toggleTab('signup')">Sign Up</button>
+    <div class="container" :class="{ 'right-panel-active': activeTab === 'signup' }">
+      <div class="form-container sign-up-container">
+        <h1>Create Account</h1>
+        <form @submit.prevent="registerUser">
+          <input type="text" placeholder="Username" v-model="signupData.username" required />
+          <input type="email" placeholder="Email" v-model="signupData.email" required />
+          <input type="password" placeholder="Password" v-model="signupData.password" required />
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+      <div class="form-container sign-in-container">
+        <h1>Sign in</h1>
+        <form @submit.prevent="loginUser">
+          <input type="email" placeholder="Email" v-model="loginData.email" required />
+          <input type="password" placeholder="Password" v-model="loginData.password" required />
+          <button type="submit">Sign In</button>
+        </form>
+      </div>
+      <div class="overlay-container">
+        <div class="overlay">
+          <div class="overlay-panel overlay-left">
+            <h1>Welcome Back!</h1>
+            <p>To keep connected with us please login with your personal info</p>
+            <button class="ghost" @click="toggleTab('login')">Sign In</button>
+          </div>
+          <div class="overlay-panel overlay-right">
+            <h1>Hello, Explorer!</h1>
+            <p>Enter your personal details and start journey with us</p>
+            <button class="ghost" @click="toggleTab('signup')">Sign Up</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</v-container>
+  </v-container>
 </template>
 
 <script>
@@ -113,20 +113,46 @@ export default {
 </script>
 
 <style scoped>
+.sign-up-container h1{
+  font-size: 25px;
+}
 
- 
+.overlay-right h1{
+  margin-top: 90px;
+  font-size: 28px;
+}
 
+.overlay-left h1{
+  margin-top: 90px;
+  font-size: 27px;
+}
 
- 
 h1 {
   font-weight: bold;
   margin: 0;
+  position: absolute;
+  top: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 10;
 }
- 
-h2 {
-  text-align: center;
+
+.sign-up-container h1 {
+  display: none;
 }
- 
+
+.right-panel-active .sign-up-container h1 {
+  display: block;
+}
+
+.sign-in-container h1 {
+  display: block;
+}
+
+.right-panel-active .sign-in-container h1 {
+  display: none;
+}
+
 p {
   font-size: 16px;
   font-weight: 100;
@@ -134,22 +160,22 @@ p {
   letter-spacing: 0.5px;
   margin: 20px 0 30px;
 }
- 
+
 span {
   font-size: 12px;
 }
- 
+
 a {
   color: #333;
   font-size: 14px;
   text-decoration: none;
   margin: 15px 0;
 }
- 
+
 button {
   border-radius: 20px;
-  border: 1px solid #ff4b2b;
-  background-color: #ff4b2b;
+  border: 1px solid #fff;
+  background-image: linear-gradient(to right top, #add8e6, #8be1e7, #71e8d8, #72edba, #90ee90);
   color: #ffffff;
   font-size: 12px;
   font-weight: bold;
@@ -158,20 +184,20 @@ button {
   text-transform: uppercase;
   transition: transform 80ms ease-in;
 }
- 
+
 button:active {
   transform: scale(0.95);
 }
- 
+
 button:focus {
   outline: none;
 }
- 
+
 button.ghost {
   background-color: transparent;
   border-color: #ffffff;
 }
- 
+
 form {
   background-color: #ffffff;
   display: flex;
@@ -182,7 +208,7 @@ form {
   height: 100%;
   text-align: center;
 }
- 
+
 input {
   background-color: #eee;
   border: none;
@@ -190,7 +216,7 @@ input {
   margin: 8px 0;
   width: 100%;
 }
- 
+
 .container {
   background-color: #fff;
   border-radius: 10px;
@@ -201,52 +227,52 @@ input {
   max-width: 100%;
   min-height: 480px;
 }
- 
+
 .form-container {
   position: absolute;
-  top: 0;
-  height: 100%;
+  top: 50px;
+  height: calc(100% - 50px);
   transition: all 0.6s ease-in-out;
 }
- 
+
 .sign-in-container {
   left: 0;
   width: 50%;
   z-index: 2;
 }
- 
+
 .container.right-panel-active .sign-in-container {
   transform: translateX(100%);
 }
- 
+
 .sign-up-container {
   left: 0;
   width: 50%;
   opacity: 0;
   z-index: 1;
 }
- 
+
 .container.right-panel-active .sign-up-container {
   transform: translateX(100%);
   opacity: 1;
   z-index: 5;
   animation: show 0.6s;
 }
- 
+
 @keyframes show {
   0%,
   49.99% {
     opacity: 0;
     z-index: 1;
   }
- 
+
   50%,
   100% {
     opacity: 1;
     z-index: 5;
   }
 }
- 
+
 .overlay-container {
   position: absolute;
   top: 0;
@@ -257,15 +283,13 @@ input {
   transition: transform 0.6s ease-in-out;
   z-index: 100;
 }
- 
+
 .container.right-panel-active .overlay-container {
   transform: translateX(-100%);
 }
- 
+
 .overlay {
-  background: #ff416c;
-  background: -webkit-linear-gradient(to right, #ff4b2b, #ff416c);
-  background: linear-gradient(to right, #ff4b2b, #ff416c);
+  background-image: linear-gradient(to right top, #add8e6, #8be1e7, #71e8d8, #72edba, #90ee90);
   background-repeat: no-repeat;
   background-size: cover;
   background-position: 0 0;
@@ -277,11 +301,11 @@ input {
   transform: translateX(0);
   transition: transform 0.6s ease-in-out;
 }
- 
+
 .container.right-panel-active .overlay {
   transform: translateX(50%);
 }
- 
+
 .overlay-panel {
   position: absolute;
   display: flex;
@@ -296,28 +320,28 @@ input {
   transform: translateX(0);
   transition: transform 0.6s ease-in-out;
 }
- 
+
 .overlay-left {
   transform: translateX(-20%);
 }
- 
+
 .container.right-panel-active .overlay-left {
   transform: translateX(0);
 }
- 
+
 .overlay-right {
   right: 0;
   transform: translateX(0);
 }
- 
+
 .container.right-panel-active .overlay-right {
   transform: translateX(20%);
 }
- 
+
 .social-container {
   margin: 20px 0;
 }
- 
+
 .social-container a {
   border: 1px solid #dddddd;
   border-radius: 50%;
@@ -328,7 +352,7 @@ input {
   height: 40px;
   width: 40px;
 }
- 
+
 footer {
   background-color: #222;
   color: #fff;
@@ -340,20 +364,17 @@ footer {
   text-align: center;
   z-index: 999;
 }
- 
+
 footer p {
   margin: 10px 0;
 }
- 
+
 footer i {
   color: red;
 }
- 
+
 footer a {
   color: #3c97bf;
   text-decoration: none;
 }
- 
 </style>
- 
- 
